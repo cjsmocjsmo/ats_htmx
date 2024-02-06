27883acf8ab9d195eaef3aadd3cc2f415394d606
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-02-06 07:50:54 -0800

    Update Docker image version to 0.0.5

--- run_docker_image.sh
+++ run_docker_image.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t ats_htmx_server:0.0.4 .;
-docker run -dit -p 9191:80 ats_htmx_server:0.0.4;
+docker build -t ats_htmx_server:0.0.5 .;
+docker run -dit -p 9191:80 ats_htmx_server:0.0.5;
