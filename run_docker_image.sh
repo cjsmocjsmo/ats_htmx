c1dd518c906d8f0f5481211f2cb71f0d1e3b4efc
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-02-05 23:43:27 -0800

    Update image URLs and Docker image version

--- run_docker_image.sh
+++ run_docker_image.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t ats_htmx_server:0.0.1 .;
-docker run -dit -p 9191:80 ats_htmx_server:0.0.1;
+docker build -t ats_htmx_server:0.0.2 .;
+docker run -dit -p 9191:80 ats_htmx_server:0.0.2;
